1c7effae7e2605d4beee0b6688da96d2487488ca
Ivana Hladová <ivuska169@gmail.com>
2022-06-07 13:24:09 +0200

    feat: switching between displays + active class to buttons

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-// premena je inicializovana(odchytila som si h1 cez jej id "heading")
-let h1 = document.getElementById("heading");
-
-// funkcia a jej nazov (parametre ktore si viem nazvat ako len chcem)
-function myFunction1(zatvorky,cislo) {
-    // vraciam vysledok (lepi parametre kedze su string ak by boli cisla tak pocita)
-    return zatvorky + cislo;  
-}
-
-// pridelujem hodnotu do vnutorneho html mojej h1-co je vysledok funkcie ( {} )7
-h1.innerHTML = myFunction1( "( {} )",7);
-
-// ==============================================================================================
-
-// funkcia je deklarovana s 3 parametrami
-function kalkulacka(prveCislo, druheCislo, znamienko) {
-    // ak je podmienka true - znamienko je plus
-    if (znamienko == "+") {
-        // tak vraciam prve cislo + druhe cislo cez KEYWORD return
-        return prveCislo + druheCislo;
-    }
-}
-// Function Invocation-vyvolanie funkcie
-// vyvolavam funkciu do konzole (cez nazov funkcie a pridanim hodnot argumentov(1,5,"+"))
-console.log(kalkulacka(1,5,"+"));
-
-// =====================================================================================================
-
 function calculateAge(birth) {
     return 2022 - birth;
 }
@@ -42,8 +14,25 @@ function yearsUntilRetirement(year, firstName) {
     } 
 }
 
-yearsUntilRetirement(2000, "Ivana");
+yearsUntilRetirement(1996, "Ivana");
 yearsUntilRetirement(1970, "Peter");
 yearsUntilRetirement(1950, "Zdena");
 
-// ===============================================================================================================
+// ==================================================================================
+
+const divContainer = document.getElementById("container");
+function myFunction(prepinak) {
+    if(prepinak == "flex"){
+        divContainer.classList.add("flex-container");
+        divContainer.classList.remove("grid-container")
+        divContainer.classList.remove("surprise-container")
+    } else if(prepinak == "grid"){
+        divContainer.classList.remove("flex-container")
+        divContainer.classList.add("grid-container");
+        divContainer.classList.remove("surprise-container")
+    } else if(prepinak == "surprise"){
+        divContainer.classList.add("surprise-container")
+        divContainer.classList.remove("grid-container")
+        divContainer.classList.remove("flex-container")
+    }
+}
